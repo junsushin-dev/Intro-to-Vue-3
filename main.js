694f60509a720fddccaf7f31131f53c8ebfc9779
junsushin-dev <junsushin4546@gmail.com>
2021-12-06 22:56:01 +0900

    feat: Add exercises for 5. List Rendering

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ const app = Vue.createApp({
       inStock: false,
       inventory: 5,
       onSale: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' }
+      ],
+      sizes: ['small', 'medium', 'large']
     }
   }
 })
