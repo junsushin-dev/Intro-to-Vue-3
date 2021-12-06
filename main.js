18d01e50ee36a7d888f8aebaa424fdb14c9a61c1
junsushin-dev <junsushin4546@gmail.com>
2021-12-06 23:02:46 +0900

    feat: Add exercises for 6. Event Handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: 'Socks',
       description: 'Beautiful Socks on Sale!',
       image: './assets/images/socks_green.jpg',
@@ -10,10 +11,21 @@ const app = Vue.createApp({
       onSale: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green' },
-        { id: 2235, color: 'blue' }
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
       ],
       sizes: ['small', 'medium', 'large']
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    updateImage(variantImage) {
+      this.image = variantImage
+    },
+    decrementFromCart() {
+      this.cart -= 1
     }
   }
 })
