47f036be50705c17b45a851245b67eff0bee2995
junsushin-dev <junsushin4546@gmail.com>
2021-12-08 22:17:19 +0900

    feat: Add exercises for 8. Computed Properties

--- main.js
+++ main.js
@@ -4,28 +4,39 @@ const app = Vue.createApp({
       cart: 0,
       product: 'Socks',
       description: 'Beautiful Socks on Sale!',
-      image: './assets/images/socks_green.jpg',
       url: 'https://naver.com',
-      inStock: false,
       inventory: 5,
       onSale: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
       ],
-      sizes: ['small', 'medium', 'large']
+      sizes: ['small', 'medium', 'large'],
+      brand: 'Vue Mastery',
+      selectedVariant: 0,
     }
   },
   methods: {
     addToCart() {
       this.cart += 1
     },
-    updateImage(variantImage) {
-      this.image = variantImage
+    updateVariant(index) {
+      this.selectedVariant = index
     },
     decrementFromCart() {
       this.cart -= 1
+    }
+  },
+  computed: {
+    title() {
+      return `${this.brand} ${this.product} ${this.onSale ? 'is on sale' : ''}`
+    },
+    image() {
+      return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity
     }
   }
 })
